a1abb4366b40db42f59b44401cdcc3ebc12462aa
MidnightKraken <99348207+MidnightKraken@users.noreply.github.com>
2023-07-31 06:33:37 -0500

    Update todolist_project.rb

--- todolist_project/lib/todolist_project.rb
+++ todolist_project/lib/todolist_project.rb
@@ -1,154 +1,263 @@
-# This class represents a todo item and its associated
-# data: name and description. There's also a "done"
-# flag to show whether this todo item is done.
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/content_for"
+require "tilt/erubis"
 
-class Todo
-  DONE_MARKER = 'X'
-  UNDONE_MARKER = ' '
+configure do 
+  enable :sessions
+  set :session_secret, "secret"
+  set :erb, :escape_html => true
+end
 
-  attr_accessor :title, :description, :done
-
-  def initialize(title, description='')
-    @title = title
-    @description = description
-    @done = false
+helpers do 
+  def list_complete?(list)
+    todos_count(list) > 0 && todos_remaining_count(list) == 0
   end
 
-  def done!
-    self.done = true
+  def list_class(list)
+    "complete" if list_complete?(list)
   end
 
-  def done?
-    done
+  def todos_count(list)
+    list[:todos].size
   end
 
-  def undone!
-    self.done = false
+  def todos_remaining_count(list)
+    list[:todos].count { |todo| !todo[:completed] }
   end
 
-  def to_s
-    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  def sort_lists(lists, &block)
+     complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
+
+    incomplete_lists.each { |list| yield list, lists.index(list)}
+    complete_lists.each  { |list| yield list, lists.index(list)}
   end
 
-  def ==(otherTodo)
-    title == otherTodo.title &&
-      description == otherTodo.description &&
-      done == otherTodo.done
+  def sort_todos(todos, &block)
+    complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed] }
+
+    incomplete_todos.each{ &block}
+    complete_todos.each{ &block}
   end
 end
 
-# This class represents a collection of Todo objects.
-# You can perform typical collection-oriented actions
-# on a TodoList object, including iteration and selection.
-
-class TodoList
-  attr_accessor :title
-
-  def initialize(title)
-    @title = title
-    @todos = []
+class SessionPersistence
+  def initialize(session)
+    @session = session
+    @session[:lists] ||= []
   end
 
-  def size
-    @todos.size
+  def find_list(id)
+    @session[:lists].find { |l| |[:id] == id }
   end
 
-  def first
-    @todos.first
+  def all_list
+    @session[:lists]
   end
 
-  def last
-    @todos.last
+  def create_new_list(list_name)
+    id = next_element_id(@session[:lists])
+    @session[:lists] << {id: id, name: list_name, todos: []}
   end
 
-  def shift
-    @todos.shift
+  def delete_list(id)
+    @session[:lists].reject! { |list| list[:id] == id }
   end
 
-  def pop
-    @todos.pop
+  def update_list_name(id, new_name)
+    list = find_list(id)
+    list[:name] = new_name
   end
 
-  def done?
-    @todos.all? { |todo| todo.done? }
+  def create_new_todo(list_id, todo_name)
+    list = find_list(list_id)
+    id = next_element_id(list[:todos])
+    list[:todos] << { id: id, name: todo_name, completed: false }
   end
 
-  def <<(todo)
-    raise TypeError, 'can only add Todo objects' unless todo.instance_of? Todo
-
-    @todos << todo
-  end
-  alias_method :add, :<<
-
-  def item_at(idx)
-    @todos.fetch(idx)
+  def delete_todo_from_list(list_id, todo_id)
+    list = find_list(list_id)
+    list[:todos].reject! { |todo| todo[:id] == todo_id }
   end
 
-  def mark_done_at(idx)
-    item_at(idx).done!
+  def update_todo_status(list_id, todo_id, new_status)
+    list = find_list(list_id)
+    todo = list[:todos].find { |t| t[:id] == todo_id }
+    todo[:completed] = new_status
   end
 
-  def mark_undone_at(idx)
-    item_at(idx).undone!
-  end
-
-  def done!
-    @todos.each_index do |idx|
-      mark_done_at(idx)
+  def mark_all_todos_as_completed(list_id)
+    list = find_list(list_id)
+    list[:todos].each do |todo|
+      todo[:completed] = true
     end
   end
 
-  def remove_at(idx)
-    @todos.delete(item_at(idx))
-  end
+    private
 
-  def to_s
-    text = "---- #{title} ----\n"
-    text << @todos.map(&:to_s).join("\n")
-    text
-  end
-
-  def to_a
-    @todos
-  end
-
-  def each
-    @todos.each do |todo|
-      yield(todo)
+    def next_element_id(elements)
+      max = elements.map { |todo| todo[:id] }.max || 0
+      max + 1
     end
-    self
   end
+  
+  def load_list(id)
+    list = @storage.find_list(id)
+    return list if list
+  
+    session[:error] = "The specified list was not found."
+    redirect "/lists"
+  end
+  
+# return an error message if the name is invalid. retuan nil if name is valid 
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "list name must be between 1-100 characters"
+ 
+  elsif session[:lists].any? {|list| list[:name] == name}
+    "list name must be unique"
+  end
+end
 
-  def select
-    list = TodoList.new(title)
-    each do |todo|
-      list.add(todo) if yield(todo)
+  def error_for_todo(name)
+      if !(1..100).cover? name.size
+      "todo name must be between 1-100 characters"
     end
-    list
   end
 
-  # returns first Todo by title, or nil if no match
-  def find_by_title(title)
-    select { |todo| todo.title == title }.first
+  before do 
+    @storage = SessionPersistence.new(session)
   end
 
-  def all_done
-    select { |todo| todo.done? }
+  get "/" do 
+    redirect "/lists"
   end
 
-  def all_not_done
-    select { |todo| !todo.done? }
+  #view list of lists
+  get "/lists" do 
+    @lists = @storage.all_lists
+    erb :lists, layout: :layout
   end
 
-  def mark_done(title)
-    find_by_title(title) && find_by_title(title).done!
+  #Render the new list form
+  get "/lists/new" do 
+    erb :new_list, layout: :layout
   end
 
-  def mark_all_done
-    each { |todo| todo.done! }
-  end
+  #Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
 
-  def mark_all_undone
-    each { |todo| todo.undone! }
+   error = error_for_list_name(list_name)
+   if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    @storage.create_new_list(list_name)
+    session[:success] = "The list has been created."
+    redirect "/lists"
   end
+end
+
+#view a single todo list 
+get "/lists/:id" do 
+  @list_id = params[:id].to_i
+  @list = @load_list(@list_id)
+  erb :list, layout: :layout
+end
+
+#edit a existing todo list 
+get "/lists/:id/edit" do
+  id = params[id].to_i
+  @list = load_list(id)
+  erb :edit_list, layout: :layout
+end
+
+#update an existing todo list 
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  id = params[id].to_i
+  @list =load_list(id)
+
+  error = error_for_list_name(list_name)
+  if error
+   session[:error] = error
+   erb :edit_list, layout: :layout
+ else
+  @storage.update_list_name(id, list_name)
+   session[:success] = "The list has been updated."
+   redirect "/lists/#{id}"
+ end
+end
+
+#delete a todo list 
+post"/lists/:id/destroy" do
+id = params[:id].to_i
+
+@storage.delete_list(id)
+
+session[:success] = "The list has been deleted."
+if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpREQUEST"
+  "/lists"
+else
+  redirect "/lists"
+ end
+end
+
+#add a new todo to a list 
+post "/lists/:list_id/todos" do 
+  @list_id = params[:list_id].to_i
+  @list = load_list(@list_id)
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error 
+    session[:error] = error
+    erb :list, layout: :layout
+  else 
+    @storage.create_new_todo(@list_id, text)
+
+    session[:success] = "The todo was added."
+    redirect "/lists/#{@list_id}"
+  end
+end
+
+#delete a todo from list 
+post "/lists/:list_id/todos/:id/destroy" do 
+  @list_id = params[:list_id].to_i
+  @list = load_list(@list_id)
+
+  todo_id = params[:id].to_i
+  @storage.delete_todo_from_list(@list_id, todo_id)
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    status 204
+  else 
+    session[:success] = "The todo has been deleted"
+    redirect "/lists/#{@list_id}"
+end
+end
+
+#update the status of a todo
+
+post "/lists/:list_id/todos/:id" do
+  @list_id = params[:list_id].to_i
+  @list = load_list(@list_id)
+
+  todo_id = params[:id].to_i
+  is_completed = params[:completed] == "true"
+  @storage.update_todo_status(@list_id, todo_id, is_completed)
+
+  session[:success] = "The todo as been updated "
+  redirect "/lists/#{@list_id}"
+end
+
+#mark all todos as complete
+post "/lists/:id/complete_all" do 
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
+ @storage.mark_all_todos_as_completed(@list_id)
+
+   session[:success] = "all todos have been completed "
+  redirect "/lists/#{@list_id}"
 end
